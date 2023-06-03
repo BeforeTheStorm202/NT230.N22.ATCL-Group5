6714abf5f1f1943b22f92786e5d84b5739aa12a6
BeforeTheStorm202 <20520191@gm.uit.edu.vn>
2023-06-03 04:11:31 -0400

    Add make_csv.py and get_strings.py to create the dataset.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 ## Malware's Modus Operandi
 Our implementation for the paper "Applying NLP techniques to malware detection in a practical environment" of Mamoru Mimura and Ryo Ito.
 
-Ref: Mimura, M., Ito, R. Applying NLP techniques to malware detection in a practical environment. Int. J. Inf. Secur. 21, 279–291 (2022). https://doi.org/10.1007/s10207-021-00553-8
+1. Construct a list of files and assign labels (make_csv.py).
+
+2. Extract strings from files with the strings command on Linux (get_strings.py).
--- make_csv.py
+++ make_csv.py
@@ -1,9 +1,10 @@
+# This file is used to create the list of file names and assign their labels in both cleanware & malware folder
 import os
 import csv
 import datetime
 
 dir_path = "/home/kali/Downloads/MalwareProject/"
-csv_path = "/home/kali/Downloads/MalwareProject/folder_info.csv"
+csv_path = "/home/kali/Downloads/NT230.N22.ATCL-Group5/folder_info.csv"
 
 # List all files in the folder and their info
 files_info = []
@@ -17,17 +18,15 @@ for folder_name in os.listdir(dir_path):
             for file_name in os.listdir(folder_path):
                 file_path = os.path.join(folder_path, file_name)
                 if os.path.isfile(file_path):  # Only consider files (not subfolders, etc.)
-                    mod_time = os.path.getmtime(file_path)  # Get modification time in seconds since epoch
-                    mod_date = str(datetime.date.fromtimestamp(mod_time))  # Convert to readable datetime format
                     if "Benign" in folder_path:
-                        label = 0
+                        label = 0 # Cleanware
                     else:
-                        label = 1
-                    files_info.append([file_path, label, mod_date])
+                        label = 1 # Malware
+                    files_info.append([file_path, label])
 
 # Write files_info to CSV file
 with open(csv_path, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Path", "Label", "Date"])  # Header row
+    writer.writerow(["File Name", "Label"])  # Header row
     for file_info in files_info:
         writer.writerow(file_info)
